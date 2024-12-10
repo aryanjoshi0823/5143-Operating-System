03c9af4554035c6a7a9fa38edc215ac1ee8b81ec
aryanjoshi0823 <ajoshi0823@my.msutexas.edu>
2024-12-10 10:12:12 -0600

    removing error files

--- Assignments/P01/cmd_pkg/clear.py
+++
@@ -1,8 +0,0 @@
-def clear(**kwargs):
-    """Clears the terminal and moves the prompt to the top-left corner."""
-
-    # use \033[H to move the cursor to the home position (top-left corner of the terminal) 
-    # and then clear the screen with \033[J. This effectively simulates clearing the 
-    # terminal and repositioning the prompt.
-    print("\033[H\033[J", end="") 
-    return ""
